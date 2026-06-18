55f920f26d00a0900865b4cf24c11406aab2acbd
max-sliva <persimon@inbox.ru>
2026-06-18 18:01:33 +0500

    first commit

---
+++ desktopApp/src/main/kotlin/org/example/project/main.kt
@@ -0,0 +1,80 @@
+package org.example.project
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+import org.jetbrains.compose.splitpane.HorizontalSplitPane
+import org.jetbrains.compose.splitpane.VerticalSplitPane
+import org.jetbrains.compose.splitpane.rememberSplitPaneState
+
+@Composable
+fun MainWindow() {
+  println("Hello world!")
+}
+
+@Composable
+fun ResizableSplitWindow() {
+    val SplitterState = rememberSplitPaneState()
+    HorizontalSplitPane (
+        splitPaneState = SplitterState
+//        modifier = Modifier.fillMaxSize(),
+//        orientation = Orientation.Vertical, // Left/Right split
+//        dividerThickness = 4.dp,
+//        initialPositionFraction = 0.3f // 30% left, 70% right
+    ) {
+        first(150.dp) {
+            Column(Modifier.fillMaxSize()) {
+                Text("Left Panel", modifier = Modifier.weight(1f))
+            }
+        }
+
+        second(120.dp) {
+            Column(Modifier.fillMaxSize()) {
+                Text("Right Panel", modifier = Modifier.weight(1f))
+            }
+        }
+        splitter {
+            visiblePart {
+                Box(
+                    Modifier
+                        .width(1.dp)
+                        .fillMaxHeight()
+                        .background(MaterialTheme.colors.background)
+                )
+            }
+            handle {
+                Box(
+                    Modifier
+                        .markAsHandle()
+//                        .cursorForHorizontalResize()
+                        .background(SolidColor(Color.Gray), alpha = 0.50f)
+                        .width(9.dp)
+                        .fillMaxHeight()
+                )
+            }
+        }
+    }
+}
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "ShiftRegisterVisualizer",
+    ) {
+//        App()
+//        MainWindow()// { /*println("Closed") */}
+        ResizableSplitWindow()
+    }
+}
